fcfa10fe1725b2df9a0fa934f1858c434937e273
Ollie Matthews <oliver.matthews@ttp.com>
2021-12-23 17:55:46 +0000

    Reprocessed word dictionary for wordle

---
+++ generate_wordle_dictionary.py
@@ -0,0 +1,27 @@
+import json
+import numpy as np
+import pickle
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def encode(word, alphabet):
+    return [alphabet.index(l) for l in word]
+
+if __name__ == '__main__':
+    with open('words_dictionary.json') as fd:
+        full_dictionary = json.load(fd)
+
+    wordle_dictionary = {key: value for key, value in full_dictionary.items() if len(key) == 5}
+
+    with open('wordle_dictionary.json', 'w') as fd:
+        json.dump(wordle_dictionary, fd)
+    
+    # Create a one hot encoding of all of the words
+    words_array = np.array([[alphabet.index(l) for l in word] for word in wordle_dictionary.keys()])
+    words_one_hot = np.zeros((words_array.shape[0], words_array.shape[1], len(alphabet)), bool)
+    for i, word in enumerate(words_array):
+        for j, letter in enumerate(word):
+            words_one_hot[i, j, letter] = True
+
+    with open('wordle_dictionary_array.p', 'wb') as fd:
+        pickle.dump(words_one_hot, fd)
